0e762ad4fb0ede81562f5843a6b6563410394586
Kenta Suzuki <kenta.suzuki@crowdworks.co.jp>
2017-05-18 09:58:52 +0900

    update help message

--- cmd/releases.go
+++ cmd/releases.go
@@ -35,13 +35,7 @@ type releasesClean struct {
 func releasesCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "releases",
-		Short: "A brief description of your command",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Short: "raiden releases command",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("releases called")
 		},
